2a1dae096068ff97c2cce9188a7678521b18c983
User Name <mckylesingh71@gmail.com>
2025-09-22 19:53:30 +0200

    Initial commit for NanaFoundation MVC app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nanas_Foundation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
