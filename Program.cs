5a7d858e1974fa35f7607dfa2f4dff46e0a307a1
User Name <mckylesingh71@gmail.com>
2025-09-23 22:42:51 +0200

    Prepared project for first migration to database

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace Nanas_Foundation
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = builder.Configuration.GetConnectionString("DB_CONNECTION_STRING") ?? throw new InvalidOperationException("Connection string 'DB_CONNECTION_STRING' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
