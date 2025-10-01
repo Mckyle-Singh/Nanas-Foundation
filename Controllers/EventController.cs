c4c218b1e9cb34831e59b676c19c033f90b1400d
User Name <mckylesingh71@gmail.com>
2025-10-01 20:52:31 +0200

    Created events controller with create and index methods

---
+++ Controllers/EventController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nanas_Foundation.Data;
+using Nanas_Foundation.Models;
+
+namespace Nanas_Foundation.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public EventController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Event evt)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Events.Add(evt);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(evt);
+        }
+
+    }
+}
