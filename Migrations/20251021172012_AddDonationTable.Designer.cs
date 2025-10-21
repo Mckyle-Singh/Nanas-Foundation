7d3e91ee35c169efa67a14807a35720a84bd8969
User Name <mckylesingh71@gmail.com>
2025-10-21 20:26:43 +0200

    Add Donation table migration and update model snapshot

--- Migrations/20251021172012_AddDonationTable.Designer.cs
+++ Migrations/20251021172012_AddDonationTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nanas_Foundation.Data;
 
@@ -11,9 +12,11 @@ using Nanas_Foundation.Data;
 namespace Nanas_Foundation.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251021172012_AddDonationTable")]
+    partial class AddDonationTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
